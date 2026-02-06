b8c2cb22aff223d04708833ef67aeceb820ea441
TheCelebrityStylist <elkejaspers.eac@gmail.com>
2026-02-06 18:04:52 +0100

    Create JsonLd.tsx

---
+++ components/JsonLd.tsx
@@ -0,0 +1,12 @@
+// components/JsonLd.tsx
+import * as React from "react";
+
+export function JsonLd({ data }: { data: Record<string, unknown> }) {
+  return (
+    <script
+      type="application/ld+json"
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+}
