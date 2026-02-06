4f022ec7acfcf47968f53209f70bd9c9aefaa973
TheCelebrityStylist <elkejaspers.eac@gmail.com>
2026-02-06 18:09:11 +0100

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,18 @@
+// app/robots.ts
+import type { MetadataRoute } from "next";
+import { site } from "@/lib/site";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        // Add any internal paths you never want indexed:
+        disallow: ["/api/", "/_next/"],
+      },
+    ],
+    sitemap: `${site.url}/sitemap.xml`,
+    host: site.url,
+  };
+}
